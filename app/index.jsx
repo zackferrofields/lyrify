0532996a5f60ce0015f884f0267f496a4e41863e
Zack Ferro-Fields <zackferrofields@gmail.com>
2015-06-21 23:03:56 +0100

    Clean up & simplify

--- app/index.jsx
+++ app/index.jsx
@@ -1,30 +1,15 @@
 import React from 'react';
-import ReactRouter from 'react-router';
+import Router from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import routes from './routes';
-import Router from './components/router';
 
 injectTapEventPlugin();
 
 function bootstrap() {
   let mountNode = document.body;
-  let AppRouter = ReactRouter.create({
-    routes: routes
-  });
-
-  AppRouter.run((Root, state) => {
-    React.render(<Root params={state.params} />, mountNode);
-  });
-
-  Router.set(AppRouter);
+  Router.run(routes, (Root, state) => { React.render(<Root params={state.params} />, mountNode); });
 }
 
 Promise.all([
-  new Promise((resolve) => {
-    if (window.addEventListener) {
-      window.addEventListener('DOMContentLoaded', resolve);
-    } else {
-      window.attachEvent('onload', resolve);
-    }
-  })
+  new Promise(resolve => { window.addEventListener('DOMContentLoaded', resolve); })
 ]).then(bootstrap);
