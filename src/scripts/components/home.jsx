8f6792ab58a199b2153f6941d4e41157d5e061b1
Zack Ferro-Fields <zackferrofields@gmail.com>
2015-06-24 21:41:05 +0100

    set initial tab index on mount

--- src/scripts/components/home.jsx
+++ src/scripts/components/home.jsx
@@ -2,34 +2,39 @@ import React from 'react';
 import {RouteHandler, State, Navigation} from 'react-router';
 import {Styles, Tabs, Tab} from 'material-ui';
 
+let tabIndex = 0;
 let ThemeManager = new Styles.ThemeManager();
+
+function getSelectedIndex(key) {
+  switch (key) {
+    default:
+    case '/home/playlists':
+      return 0;
+    case '/home/explore':
+      return 1;
+  }
+}
 
 export default React.createClass({
   mixins: [State, Navigation],
   childContextTypes: { muiTheme: React.PropTypes.object },
+  componentWillMount() {
+    tabIndex = getSelectedIndex(this.getPathname());
+  },
   getChildContext() {
     return {
       muiTheme: ThemeManager.getCurrentTheme()
     };
   },
-  getSelectedIndex(key) {
-    switch (key) {
-      default:
-      case '/home/playlists':
-        return 0;
-      case '/home/explore':
-        return 1;
-    }
-  },
   onActive(tab) {
+    tabIndex = tab.props.tabIndex;
     this.transitionTo(tab.props.route);
   },
   render() {
     let name = this.getPathname();
-    let selected = this.getSelectedIndex(name);
     return (
       <section>
-        <Tabs initialSelectedIndex={selected}>
+        <Tabs initialSelectedIndex={tabIndex}>
           <Tab label="Playlists" onActive={this.onActive} route="/home/playlists"/>
           <Tab label="Explore" onActive={this.onActive} route="/home/explore"/>
         </Tabs>
