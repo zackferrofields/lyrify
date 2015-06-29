c231dfee623283d48787347c4e50ab221f170378
Zack Ferro-Fields <zackferrofields@gmail.com>
2015-06-30 00:07:52 +0100

    fix youtube getInfo issue

--- src/scripts/api/youtube-download.js
+++ src/scripts/api/youtube-download.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import ytdl from 'ytdl-core';
 
-const url = 'http://youtube.com/watch';
+const url = 'https://www.youtube.com/watch';
 
 /**
 * Parse Video Object to url
@@ -20,30 +20,26 @@ function parseVideo(video) {
 }
 
 export default function(video) {
-  let self = this;
   let uri = parseVideo(video);
   return new Promise((resolve, reject) => {
-    ytdl.getInfo(uri, {downloadURL: true}, (err, info) => {
+    ytdl.getInfo(uri, (err, info) => {
       if (err) {
         return reject(err);
       }
       let audioFormats = info.formats.filter(format => format.container && format.type.startsWith('audio'));
       if (!audioFormats.length) {
-        return reject(new Error(`${url} doesn't contain an audio format`));
+        return reject(new Error(`${uri} doesn't contain an audio format`));
       }
-      let title = info.title.replace(/ /g, '-');
+      let name = info.title.replace(/\W/g, '');
       let audioFormat = audioFormats.reduce((acc, audio) => audio.audioBitrate > acc.audioBitrate ? audio : acc, { audioBitrate: 0 });
-      let fileName = `${title}.${audioFormat.container}`;
-      let fileSize = 1;
-      let dataRead = 0;
-      ytdl.downloadFromInfo(info, {
-        format: audioFormat
-      })
-      .on('error', reject.bind(null, fileName))
-      .on('format', formatInfo => fileSize = formatInfo.size)
-      .on('data', data => self.progressed(fileName, (dataRead += data.length) / fileSize))
-      .on('end', resolve.bind(null, fileName))
-      .pipe(fs.createWriteStream(`./app/sounds/${fileName}`));
+      let filename = `${name}.${audioFormat.container}`;
+      let location = `./app/resources/sounds/${filename}`;
+      ytdl(info.loaderUrl, { format: audioFormat })
+      .on('error', reject.bind(null, info))
+      // .on('format', formatInfo => fileSize = formatInfo.size)
+      // .on('data', data => self.progressed(fileName, (dataRead += data.length) / fileSize))
+      .on('end', resolve.bind(null, info))
+      .pipe(fs.createWriteStream(location));
     });
   });
 }
