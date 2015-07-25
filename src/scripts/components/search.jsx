557d168196aa526dc5c22502d3911d4325ea7325
Zack Ferro-Fields <zackferrofields@gmail.com>
2015-07-25 20:55:38 +0100

    Observable next sequence

--- src/scripts/components/search.jsx
+++ src/scripts/components/search.jsx
@@ -6,20 +6,21 @@ import Actions from '../actions';
 import {Styles, TextField, IconButton} from 'material-ui';
 import Icons from 'icons';
 
-const ENTER_KEY = 13;
 let ThemeManager = new Styles.ThemeManager();
 
 export default React.createClass({
   mixins: [Reflux.connect(Search, 'results')],
   childContextTypes: { muiTheme: React.PropTypes.object },
   componentDidMount() {
-    let events = Rx.Observable.fromEvent(this.refs.search._getInputNode(), 'keypress');
-    let enter = events.filter( event => event.keyCode === ENTER_KEY);
-    let notEnter = events.filter( event => event.keyCode !== ENTER_KEY).throttle(500);
-    Rx.Observable.merge(enter, notEnter)
+    Rx.Observable.fromEvent(this.refs.search._getInputNode(), 'keypress')
+      .throttle(500)
       .map(event => event.target.value.trim())
       .filter(query => query !== '')
       .flatMapLatest(query => Rx.Observable.fromPromise(Actions.searchYouTube(query)))
+      .merge(
+        Rx.Observable.fromEvent(React.findDOMNode(this.refs.next), 'click')
+          .flatMapLatest(() => Rx.Observable.fromPromise(Actions.searchYouTube(this.refs.search.getValue(), this.state.results.nextPageToken)))
+      )
       .pluck('items')
       .concatMap(items => {
         let promises = items.map(item => Actions.infoYouTube(item.id.videoId));
@@ -35,10 +36,6 @@ export default React.createClass({
   onFocus() {
     this.refs.search.focus();
   },
-  onNext() {
-    if (!this.state.results.nextPageToken) return;
-    Actions.searchYouTube(this.refs.search.getValue(), this.state.results.nextPageToken);
-  },
   onPrevious() {
     if (!this.state.results.prevPageToken) return;
     Actions.searchYouTube(this.refs.search.getValue(), this.state.results.prevPageToken);
@@ -48,8 +45,7 @@ export default React.createClass({
       <div>
         <IconButton className={Icons.search} onClick={this.onFocus}/>
         <TextField hintText="Search" ref="search"/>
-        <IconButton className={Icons.previous} disabled={!this.state.results.prevPageToken} onClick={this.onPrevious}/>
-        <IconButton className={Icons.next} disabled={!this.state.results.nextPageToken} onClick={this.onNext}/>
+        <IconButton className={Icons.next} disabled={!this.state.results.nextPageToken} ref="next"/>
       </div>
     );
   }
